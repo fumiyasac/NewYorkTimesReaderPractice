ccff3af526dcaa06a34ceeac84d85a93850c0a1d
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2017-03-20 21:28:13 +0900

    [WIP] Add comment App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+/**
+ * アプリ構築用のファイル
+ */
 import React from 'react';
+
+//ProviderタグでラップすることでReactコンポーネント内でStoreにアクセスできるようにする
 import { Provider } from 'react-redux';
+
+//自作コンポーネントのインポート
 import HomeScreen from './components/HomeScreen';
+
+//createStoreのインポート
 import createStore from './createStore';
 
+//初期状態のステートを取得する
 const store = createStore();
 
+//アプリ画面の組み立て
 export default () => (
   <Provider store={store}>
     <HomeScreen />
