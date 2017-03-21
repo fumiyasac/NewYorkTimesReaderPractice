5ec00b1a3c757a74e839cd65da45366a49091850
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2017-03-21 23:34:07 +0900

    [WIP] Add action type setting.

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -3,6 +3,13 @@
  * 該当の定数で定義されたアクションを実行することでステートを更新する
  */
 
-//アクションタイプ定義
+//ニュースデータの表示に関するアクション名一覧
 export const LOAD_NEWS = 'LOAD_NEWS';
 export const SEARCH_NEWS = 'SEARCH_NEWS';
+
+//ナビゲーター及び画面遷移に関するアクション名一覧
+export const NAVIGATION_PUSH = 'NAVIGATION_PUSH';
+export const NAVIGATION_POP = 'NAVIGATION_POP';
+export const NAVIGATION_TAB = 'NAVIGATION_TAB';
+export const NAVIGATION_OPEN_MODAL = 'NAVIGATION_OPEN_MODAL';
+export const NAVIGATION_CLOSE_MODAL = 'NAVIGATION_CLOSE_MODAL';
