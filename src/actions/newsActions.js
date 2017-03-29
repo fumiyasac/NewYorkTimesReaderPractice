80a4d7ded2eacc63a6b65e20ba7efadd1c35669f
Fumiya Sakai <just1factory@gmail.com>
2017-03-29 23:51:08 +0900

    [WIP] newsAction.js & test fix

--- src/actions/newsActions.js
+++ src/actions/newsActions.js
@@ -9,19 +9,15 @@ import { LOAD_NEWS, LOAD_NEWS_MOCK, SEARCH_NEWS } from './actionTypes';
 //APIキーを呼び出す
 import API_KEY from '../config/apiKey';
 
-//モックデータになるNewYorkTimesのニュースデータ一覧を取得する（引数なし）
-/*
-import mockData from '../mockData.json';
-
-export const loadNews = () => ({
-  type: LOAD_NEWS,
-  payload: mockData
-});
-*/
-
-//NewYorkTimesのニュースデータ一覧を取得する（引数なし）
-export const loadNews = () => {
+//NewYorkTimesのニュースデータ一覧を取得する（APIからのfetch部分）
+export const loadNewsFromAPI = () => {
   const req = fetch(`https://api.nytimes.com/svc/topstories/v2/food.json?api-key=${API_KEY}`);
+  return req;
+};
+
+//NewYorkTimesのニュースデータ一覧を取得する
+export const loadNews = () => {
+  const req = loadNewsFromAPI();
   return {
     type: LOAD_NEWS,
     payload: req.then(response => response.json())
--- src/actions/newsActions.test.js
+++ src/actions/newsActions.test.js
@@ -1,5 +1,5 @@
 import { LOAD_NEWS, LOAD_NEWS_MOCK, SEARCH_NEWS } from './actionTypes';
-import { loadNews, loadNewsMock, searchNews } from './newsActions';
+import { loadNews, loadNewsFromAPI, searchNews } from './newsActions';
 
 //テストコード内でfetchを実行できるようにする
 import fetch from 'isomorphic-fetch';
@@ -7,27 +7,11 @@ import fetch from 'isomorphic-fetch';
 //APIキー
 import API_KEY from '../config/apiKey';
 
-/*
-import mockData from '../mockData.json';
-*/
-
-//非同期処理の結果オブジェクトのモック(Promiseでのデータが渡るまでのテストなのでこれでいったんOK)
-const promiseResultMock = {"_45": 0, "_54": null, "_65": null, "_81": 0};
-
 describe('newsActionsに関連するテスト', () => {
 
-  /*
-  test('loadNewsMockアクションにてモックが渡されること', () => {
-    const mock = mockData;
-    expect(loadNews()).toEqual(
-      { type: LOAD_NEWS, payload: mock }
-    );
-  });
-  */
-
-  test('loadNewsアクション内で実行されるfetchメソッドのレスポンスが正しく返却されること', async () => {
+  test('loadNewsFromAPIアクション内で実行されるfetchメソッドのレスポンスが正しく返却されること', async () => {
     //loadNews内で使用されているfetchメソッドの部分を抜き出す
-    const request = () => fetch(`https://api.nytimes.com/svc/topstories/v2/food.json?api-key=${API_KEY}`);
+    const request = () => loadNewsFromAPI();
     const response = await request();
     //APIのレスポンスが正しいかをテストする
     expect(response).not.toBe(undefined);
@@ -35,10 +19,11 @@ describe('newsActionsに関連するテスト', () => {
     expect(response.ok).toBe(true);
   });
 
-  test('loadNewsアクション内で実行されるfetchメソッドのレスポンスと引数の値がpayloadに渡されること', async () => {
-    const action = await loadNews();
-    expect(action).toEqual(
-      { type: LOAD_NEWS, payload: promiseResultMock }
+  test('loadNewsアクション内でPromiseオブジェクトが渡されること', () => {
+    //Promiseオブジェクトのダミー
+    let dummyPromiseObject = {"_45": 0, "_54": null, "_65": null, "_81": 0};
+    expect(loadNews()).toEqual(
+      { type: LOAD_NEWS, payload: dummyPromiseObject }
     );
   });
 
